65f162ebb23e11888fc110554de3ee121431e185
Jack O. Wasey <jack@jackwasey.com>
2018-08-08 12:31:36 -0400

    travis again, don't run tests that rely on suggested packages which travis can't make available.

--- R/prisma.r
+++ R/prisma.r
@@ -202,9 +202,10 @@ pnl <- function(...)
 #' }
 #' @keywords internal
 prisma_pdf <- function(x, filename = "prisma.pdf") {
-  utils::capture.output({
-    rsvg::rsvg_pdf(svg = charToRaw(DiagrammeRsvg::export_svg(x)),
-                   file = filename)
-  })
+  if (require(DiagrammeRsvg) && require(rsvg))
+    utils::capture.output({
+      rsvg::rsvg_pdf(svg = charToRaw(DiagrammeRsvg::export_svg(x)),
+                     file = filename)
+    })
   invisible()
 }
