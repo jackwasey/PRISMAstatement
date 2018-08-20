1ccd0a7d656013f1979cf4480b5c56237a36c850
Jack O. Wasey <jack@jackwasey.com>
2018-08-20 05:32:07 -0400

    Updated vignette text from paper. Ignore built vignette documents in inst/doc. Drop abstracts and keywords from merged bibliography used in vignette. Merging bibs due to https://github.com/rstudio/rmarkdown/issues/1358 . (JOSS paper.md can handle two bib files, however, because the bot calls pandoc directly and correctly).

--- bin/mergebib.sh
+++ bin/mergebib.sh
@@ -4,7 +4,10 @@
 #cat vignettes/other.bib >> vignettes/all.bib
 #cat vignettes/gplv3.bib >> vignettes/all.bib
 
-command -v bibtool >/dev/null 2>&1 || { echo >&2 "I require bibtool but it's not installed. Aborting."; exit 1; }
+command -v bibtool >/dev/null 2>&1 || {
+  echo >&2 "I require bibtool but it's not installed. Aborting."
+  exit 1
+}
 
 if [ -d vignettes ]; then
   pushd vignettes
@@ -17,17 +20,9 @@ bibtool -s -r biblatex r.bib PRISMA.bib -- preserve.key.case=on > all.bib
 if grep '[^\]_' all.bib | grep -v ^@
 then
   echo "Underscores found in all.bib entries"
-
   echo "attempting to fix"
-  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
-  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
-  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
-  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
-  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
-  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
-  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
-  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
-  exit 1
+  for run in {1..25}
+  do
+    perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
+  done
 fi
-
-
