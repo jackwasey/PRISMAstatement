7d6df1d69ab58be296e01502bd181ef753f3402a
Eero <eero@kuusisto.de>
2020-01-06 23:00:39 +0100

    Added an apostrohe test for labels

--- tests/testthat/test-prisma.r
+++ tests/testthat/test-prisma.r
@@ -90,6 +90,21 @@ test_that("change a label", {
     regexp = NA)
 })
 
+test_that("use an apostrophe in a label", {
+  expect_error(
+    prisma(found = 0,
+           found_other = 0,
+           no_dupes = 0,
+           screened = 0,
+           screen_exclusions = 0,
+           full_text = 0,
+           full_text_exclusions = 0,
+           qualitative = 0,
+           quantitative = 0,
+           labels = list(screened = "PLEASE DON'T RETURN AN ERROR")),
+    regexp = NA)
+})
+
 test_that("numbers must work together", {
   expect_error(prisma(1, 2, 3, 4, 5, 6, 7, 8, 9))
   expect_warning(prisma(1000, 20, 270, 270, 10, 260, 19, 240, 107))
